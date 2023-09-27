33a0f32f9ae6b070896346aec3408ec4cccb54be
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-09-27 16:14:04 -0700

    add patch

--- sst.config.ts
+++ sst.config.ts
@@ -1,43 +1,18 @@
 import { SSTConfig } from "sst";
-import { Cron, Bucket, NextjsSite as SSTNext } from "sst/constructs";
-import { Duration, Stack } from "aws-cdk-lib/core";
+import { NextjsSite } from "sst/constructs";
 
-import {
-  CacheCookieBehavior,
-  CacheHeaderBehavior,
-  CachePolicy,
-  CacheQueryStringBehavior,
-} from "aws-cdk-lib/aws-cloudfront";
-function buildServerCachePolicy(stack: Stack) {
-  return new CachePolicy(stack, "ServerCache", {
-    queryStringBehavior: CacheQueryStringBehavior.all(),
-    headerBehavior: CacheHeaderBehavior.allowList(
-      ...[
-        "accept",
-        "rsc",
-        "next-router-prefetch",
-        "next-router-state-tree",
-        "next-url",
-        "x-invoke-output",
-        "x-invoke-path",
-        "x-invoke-query",
-        "x-middleware-invoke",
-      ]
-    ),
-    cookieBehavior: CacheCookieBehavior.none(),
-    defaultTtl: Duration.days(0),
-    maxTtl: Duration.days(365),
-    minTtl: Duration.days(0),
-    enableAcceptEncodingBrotli: true,
-    enableAcceptEncodingGzip: true,
-    comment: "Nextjs Server response cache policy",
-  });
-}
-class NextjsSite extends SSTNext {
-  protected supportsStreaming(): boolean {
-    return true;
-  }
-}
+// import {
+//   CacheCookieBehavior,
+//   CacheHeaderBehavior,
+//   CachePolicy,
+//   CacheQueryStringBehavior,
+// } from "aws-cdk-lib/aws-cloudfront";
+
+// class NextjsSite extends SSTNext {
+//   protected supportsStreaming(): boolean {
+//     return false;
+//   }
+// }
 export default {
   config() {
     return {
@@ -53,7 +28,6 @@ export default {
         path: ".",
         buildCommand: "npm run openbuild",
         cdk: {
-          serverCachePolicy: buildServerCachePolicy(stack),
         },
       });
 
